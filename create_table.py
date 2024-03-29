a218ce5ac020eaaeaaa19e1de9e0b40dcd4269f1
Mostafa Ghanem <mg656688@gmail.com>
2024-03-29 06:35:46 +0200

    Fix login and create endpoints

--- create_table.py
+++ create_table.py
@@ -1,1 +1,25 @@
+from sqlalchemy import create_engine, text
 
+# Assuming 'sqlite:///your_database_file.sqlite' is the URI for your SQLite database
+engine = create_engine('sqlite:///dp5.sqlite')
+
+# Establish a connection to the database
+conn = engine.connect()
+
+# Define the SQL query as a string
+sql_query = ('\n'
+             '    CREATE TABLE predictions (\n'
+             '        id INTEGER PRIMARY KEY,\n'
+             '        name TEXT,\n'
+             '        place TEXT,\n'
+             '        latitude FLOAT,\n'
+             '        longitude FLOAT,\n'
+             '        duration INTEGER,\n'
+             '        user_id INTEGER\n'
+             '    )\n')
+
+# Execute the SQL query to create the table
+conn.execute(text(sql_query))
+
+# Close the connection
+conn.close()
