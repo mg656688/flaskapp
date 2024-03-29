bc60b74ef3fcdb88b9237315c61964d848c18312
ali-hammada <116171346+ali-hammada@users.noreply.github.com>
2024-03-28 19:16:30 -0700

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addToCartButtons = document.querySelectorAll(".add-to-cart");
-    const cartCount = document.getElementById("cart-count");
 
-    let cartItems = 0;
-
-    addToCartButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            cartItems++;
-            cartCount.textContent = cartItems;
-        });
-    });
-});
